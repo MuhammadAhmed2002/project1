c5029b5f64caf24ba20f101fdb3c0de2daa26980
MuhammadAhmed2002 <81912326+MuhammadAhmed2002@users.noreply.github.com>
2021-05-19 23:32:48 +0500

    Add files via upload

---
+++ project1/Gui/src/gui/Gui.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author M AHMAD
+ */
+public class Gui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form1 F = new Form1();
+        F.setVisible(true);
+    }
+    
+    
+}
